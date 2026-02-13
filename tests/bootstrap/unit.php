affff84b6db007abc22fc4ca00a1d598154138fc
Seiji Amashige <s-amashige@stf.kodansha.co.jp>
2026-02-14 03:59:53 +0900

    bootstrap を unit/integration に分離 #20

---
+++ tests/bootstrap/unit.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Bootstrap for unit tests
+ *
+ * Unit tests use Brain Monkey for mocking WordPress functions.
+ * No WordPress installation required.
+ */
+
+require_once __DIR__ . '/../../vendor/autoload.php';
