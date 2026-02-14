dd7a2f8c6e5d92c1f3887b13b4a6d1d03f5ed9d6
Seiji Amashige <s-amashige@stf.kodansha.co.jp>
2026-02-14 15:41:54 +0900

    test の bootstrap ファイルを一つにする #20

--- tests/bootstrap/integration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Bootstrap for integration tests
- *
- * Integration tests use Brain Monkey for mocking WordPress functions.
- * Tests verify component interactions without requiring WordPress installation.
- */
-
-require_once __DIR__ . '/../../vendor/autoload.php';
