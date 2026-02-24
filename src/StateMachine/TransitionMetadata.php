74ee349971129a966720092dd21e88384616971a
amashigeseiji <tenjuu99@gmail.com>
2026-02-25 01:11:08 +0900

    コメントを追加する

--- src/StateMachine/TransitionMetadata.php
+++ src/StateMachine/TransitionMetadata.php
@@ -6,21 +6,23 @@ use Wp\Resta\StateMachine\Transition;
 trait TransitionMetadata
 {
     /**
-     * @return array<string, static>
+     * Transition アトリビュートから action → from state のマッピングを取得します。
+     *
+     * このメソッドは enum に定義されたすべての Transition を走査し、
+     * アクション名をキー、遷移元の状態を値とする配列を返します。
+     * 結果は静的にキャッシュされます。
+     *
+     * @return array<string, static> アクション名 => 遷移元状態のマップ
      */
     public static function actions(): array
     {
-        static $result;
-        if (!$result) {
-            $result = [];
-        }
+        static $result = [];
         if (isset($result[static::class])) {
             return $result[static::class];
         }
         $result[static::class] = [];
         foreach ((new \ReflectionEnum(static::class))->getCases() as $case) {
             foreach ($case->getAttributes(Transition::class) as $attr) {
-                /** @var self */
                 $value = $case->getValue();
                 $result[static::class][$attr->newInstance()->on] = $value;
             }
